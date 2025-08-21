fefa73b180251ded37a11566a55fb7d18e5e39fc
Admin <alexr.fournier@gmail.com>
2025-08-21 18:48:57 -0500

    Clean repo and add to .gitignore

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,11 @@
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body style={{ fontFamily: 'sans-serif', padding: '20px' }}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
